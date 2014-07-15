aae7a5b9a9cd3ed8eddc30745b91f9f6bd920fd0
Dan Chen <dchen@thoughtworks.com>
2014-07-15 10:10:12 +0800

    gradle

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,11 +5,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HelloWorld{
-    private static final Logger logger=LoggerFactory.getLogger(HelloWorld.class);
+    //public static final
 
     public  void SayHello(){
+        Logger logger=LoggerFactory.getLogger(HelloWorld.class);
         // logger =
         logger.info("Hello World");
+        logger.trace("======trace");
+        logger.debug("======debug");
+        logger.info("======info");
+        logger.warn("======warn");
+        logger.error("======error");
         //return "hello";
     }
+
+   /* private static Logger log = LoggerFactory.getLogger(HelloWorld.class);
+    public static void main(String[] args) {
+        log.trace("======trace");
+        log.debug("======debug");
+        log.info("======info");
+        log.warn("======warn");
+        log.error("======error");
+        System.out.print("test");
+    } */
 }
---
+++ src/main/java/hello/HelloWorldServlet.java
@@ -0,0 +1,35 @@
+package hello;
+import java.io.IOException;
+
+/**
+ * Created by dchen on 7/15/14.
+ */
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+public class HelloWorldServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        //super.doGet(req, resp);
+        PrintWriter pw  = resp.getWriter();//得到一个输出流
+        //写给Client端一个简单网页信息
+        pw.println("<html><head></head><body>Server:Hello Client~</body></html>");
+        pw.flush();
+        pw.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        super.doPost(req, resp);
+    }
+}
