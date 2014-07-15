9bf879fb461df1d7d7b9f4d790e3c19237b5b6a3
Dan Chen <dchen@thoughtworks.com>
2014-07-15 11:44:42 +0800

    web

---
+++ core/src/test/java/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void testSayHello() {
+
+    }
+
+
+}
--- web/src/main/java/hello/HelloWorldServlet.java
+++ web/src/main/java/hello/HelloWorldServlet.java
@@ -1,5 +1,4 @@
 package hello;
-import hello.HelloWorld;
 
 import java.io.IOException;
 
@@ -10,7 +9,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 public class HelloWorldServlet extends HttpServlet {
 
@@ -19,21 +17,10 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        //super.doGet(req, resp);
         PrintWriter pw  = resp.getWriter();//得到一个输出流
-        HelloWorld helloWorld=new HelloWorld();
-        String str=helloWorld.SayHello();
-        //写给Client端一个简单网页信息
-        pw.println("<html><head></head><body>"+str+"~</body></html>");
+        pw.println("<html><head></head><body>"+ new HelloWorld().sayHello() +"~</body></html>");
         pw.flush();
         pw.close();
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doPost(req, resp);
-    }
 }
