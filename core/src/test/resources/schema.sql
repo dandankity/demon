46de066f7303afa85e65933770395e65f04a3a2a
Dan Chen <dchen@thoughtworks.com>
2015-08-23 19:42:12 -0700

    remove files

--- core/src/test/resources/schema.sql
+++
@@ -1,3 +0,0 @@
-create table employee(EmployeeID varchar(5), Name varchar(20),Exp double,Budget double);
-create table budget(ExpenseID varchar(10),EmployeeID varchar(5),Name varchar(20),ExpenseType varchar(50),
-                ExpenseDate date,Cost double,Description varchar(100));
